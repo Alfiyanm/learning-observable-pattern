93abedbf05c2a8003957ecdb1fe938d41674a336
Muchlas Alfiyan <muchlas12@gmail.com>
2019-05-01 12:54:10 +0700

    add event-bus-experiments component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserEventExperimentsComponent } from './browser-event-experiments/browser-event-experiments.component';
+import { EventBusExperimentsComponent } from './event-bus-experiments/event-bus-experiments.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BrowserEventExperimentsComponent
+    BrowserEventExperimentsComponent,
+    EventBusExperimentsComponent
   ],
   imports: [
     BrowserModule
